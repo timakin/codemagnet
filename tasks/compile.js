cde3ef90c219ca59c2af8a20fd7d607208bd909a
timakin <timaki.st@gmail.com>
2015-03-02 00:50:13 +0900

    Add: gulp-react

--- tasks/compile.js
+++ tasks/compile.js
@@ -2,12 +2,17 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var less = require('gulp-less');
 var minify = require('gulp-minify-css');
+var react = require('gulp-react');
 var confPath = require('../config.json').paths;
 
 module.exports = {
 
   jsx: function(){
     console.log("jsx");
+    return gulp.src(confPath.resource.react)
+      .pipe(concat('bundle.jsx'))
+      .pipe(react())
+      .pipe(gulp.dest(confPath.dest.react));
   },
 
   less: function(){
