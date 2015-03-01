8abc29a6c136137a552408184f81eeec25554eb1
timakin <timaki.st@gmail.com>
2015-03-01 21:00:44 +0900

    temp push

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,15 @@
 var gulp = require('gulp');
+var less = require('gulp-less');
+var path = require('path');
 
 gulp.task('default', function(){
   console.log('default tasks');
+});
+
+gulp.task('less', function () {
+  return gulp.src('./client/**/*.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
+    .pipe(gulp.dest('./public/style'));
 });
