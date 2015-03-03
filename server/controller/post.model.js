c4d8729dd0a21f344d25255ae2403a211b9fefac
timakin <timaki.st@gmail.com>
2015-03-03 11:23:14 +0900

    Fix: PostSchema spell miss

--- server/controller/post.model.js
+++ server/controller/post.model.js
@@ -1,13 +1,13 @@
 var mongoose = require('mongoose');
 
 var PostSchema = new mongoose.Schema({
-    user_id: { type: String},
     code: {type: String},
     description: {type: String},
-    section: {type:String},
+    section: {type: String},
+    user_id: {type: String},
     created:    { type: Date, default: Date.now },
     updated:    { type: Date, default: Date.now }
 });
 
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model('Post', PostSchema);
