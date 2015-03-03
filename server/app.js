63ca0fc47087b023a8298d4638e7939abd26be4a
timakin <timaki.st@gmail.com>
2015-03-03 18:17:57 +0900

    Add: server/config dir

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var connectDB = require('./database');
+var connectDB = require('./config/database');
 
 // ===== view engine setup
 // uncomment after placing your favicon in /public
--- server/config/database.js
+++ server/config/database.js
@@ -2,6 +2,6 @@ var mongoose = require('mongoose');
 var path = require('path');
 
 module.exports = function(){
-  var config = require(path.join(__dirname, '../config.json'));
+  var config = require(path.join(__dirname, '../../config.json'));
   mongoose.connect(config.mongoUrl);
 };
