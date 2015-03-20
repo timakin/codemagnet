79069d517825184bb18c26b10091e30b00114097
timakin <timaki.st@gmail.com>
2015-03-20 15:04:18 +0900

    [Add]Docker and fig

--- docker/setup.sh
+++
@@ -1,7 +0,0 @@
-cd /src
-rm -rf codemagnet; true
-git clone https://github.com/timakin/codemagnet
-cd codemagnet
-npm install
-bower install
-gulp serve
--- docker/start.sh
+++ docker/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 boot2docker up
-
-docker build -t timakin/codemagnet ./
-docker run -it -p 80:3000 --name magnet timakin/codemagnet /bin/bash
+fig up -d
+#docker build -t timakin/codemagnet ./
+#docker run -it -p 80:3000 --name magnet timakin/codemagnet /bin/bash
