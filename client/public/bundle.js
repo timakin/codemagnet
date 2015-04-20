db29cff42d0867f0107784ff9b25978a3906028e
Seiji Takahashi <seiji.takahashi@O-05467-MAC.local>
2015-04-20 18:34:12 +0900

    revert docker setting

--- client/public/bundle.js
+++ client/public/bundle.js
@@ -166,7 +166,6 @@ process.browser = true;
 process.env = {};
 process.argv = [];
 process.version = ''; // empty string to avoid regexp issues
-process.versions = {};
 
 function noop() {}
 
@@ -21245,7 +21244,7 @@ var reduce = require('reduce');
  */
 
 var root = 'undefined' == typeof window
-  ? (this || self)
+  ? this
   : window;
 
 /**
@@ -21284,8 +21283,7 @@ function isHost(obj) {
 
 request.getXHR = function () {
   if (root.XMLHttpRequest
-      && (!root.location || 'file:' != root.location.protocol
-          || !root.ActiveXObject)) {
+    && ('file:' != root.location.protocol || !root.ActiveXObject)) {
     return new XMLHttpRequest;
   } else {
     try { return new ActiveXObject('Microsoft.XMLHTTP'); } catch(e) {}
@@ -21621,11 +21619,6 @@ Response.prototype.parseBody = function(str){
  */
 
 Response.prototype.setStatusProperties = function(status){
-  // handle IE9 bug: http://stackoverflow.com/questions/10046972/msie-returns-status-code-of-1223-for-ajax-request
-  if (status === 1223) {
-    status = 204;
-  }
-
   var type = status / 100 | 0;
 
   // status / class
@@ -21643,7 +21636,7 @@ Response.prototype.setStatusProperties = function(status){
 
   // sugar
   this.accepted = 202 == status;
-  this.noContent = 204 == status;
+  this.noContent = 204 == status || 1223 == status;
   this.badRequest = 400 == status;
   this.unauthorized = 401 == status;
   this.notAcceptable = 406 == status;
@@ -22151,18 +22144,12 @@ Request.prototype.end = function(fn){
   };
 
   // progress
-  var handleProgress = function(e){
-    if (e.total > 0) {
-      e.percent = e.loaded / e.total * 100;
-    }
-    self.emit('progress', e);
-  };
-  if (this.hasListeners('progress')) {
-    xhr.onprogress = handleProgress;
-  }
   try {
     if (xhr.upload && this.hasListeners('progress')) {
-      xhr.upload.onprogress = handleProgress;
+      xhr.upload.onprogress = function(e){
+        e.percent = e.loaded / e.total * 100;
+        self.emit('progress', e);
+      };
     }
   } catch(e) {
     // Accessing xhr.upload fails in IE from a web worker, so just pretend it doesn't exist.
