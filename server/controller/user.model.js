eb8c8836fdb2a1505ec621f673bb2a128d9bcc12
timakin <timaki.st@gmail.com>
2015-04-24 21:20:57 +0900

    Deleted unnecessary space

--- server/controller/user.model.js
+++ server/controller/user.model.js
@@ -4,8 +4,8 @@ var UserSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
-    created:    { type: Date, default: Date.now },
-    updated:    { type: Date, default: Date.now }
+    created: { type: Date, default: Date.now },
+    updated: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('User', UserSchema);
