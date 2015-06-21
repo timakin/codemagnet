0970d386b869673e110fd12b005a31fd09262b18
timakin <timaki.st@gmail.com>
2015-06-21 19:13:34 +0900

    className

--- client/components/home/home.js
+++ client/components/home/home.js
@@ -8,9 +8,9 @@ const Home = React.createClass({
             <h1>this is home</h1>
             <h1>Go to <Link to="/post">Post</Link></h1>
             <form action="/auth/login" method="POST">
-              <input type="text" name="name" class='form-control' />
-              <input type='password' name='password' class='form-control' />
-              <button class='btn btn-lg btn-primary btn-block' type='submit' />
+              <input type="text" name="name" className='form-control' />
+              <input type='password' name='password' className='form-control' />
+              <button className='btn btn-lg btn-primary btn-block' type='submit' />
             </form>
             <a href="/register">Register</a>
           </div>
