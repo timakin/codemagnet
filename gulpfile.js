caab3c74f8daa7402e9e9a2fcf74fd2750a079d6
timakin <timaki.st@gmail.com>
2015-06-21 18:09:30 +0900

    gulpifなど

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp       = require('gulp');
 var concat     = require('gulp-concat');
 var less       = require('gulp-less');
+var gulpif     = require('gulp-if');
 var minify     = require('gulp-minify-css');
 var nodemon    = require('gulp-nodemon');
 var plumber    = require('gulp-plumber');
@@ -13,25 +14,27 @@ var browserify = require('browserify');
 var path       = require('path');
 var source     = require('vinyl-source-stream');
 
-var config     = require('./config.json').build;
+var config        = require('./config.json').build;
+var isDevelopment = !process.env.NODE_ENV ||
+                     process.env.NODE_ENV === 'development';
 
 gulp.task('bundle:js', function() {
   browserify({
-      entries: [config.resource.scripts],
+      entries: config["JS_ENTRY_POINT"],
       debug: true,
       extensions: config["EXTENSIONS"],
   })
   .transform(babelify)
   .bundle()
-  .pipe(plumber())
   .pipe(source('bundle.js'))
-  .pipe(streamify(uglify()))
+  .pipe(gulpif(isDevelopment, plumber()))
+  .pipe(gulpif(!isDevelopment, streamify(uglify())))
   .pipe(gulp.dest(config["DEST_DIR_PATH"]))
 });
 
 gulp.task('bundle:stylesheet', function() {
-  gulp.src(config.resource.styles)
-    .pipe(plumber())
+  gulp.src(config["STYLESHEET_ENTRY_POINT"])
+    .pipe(gulpif(isDevelopment, plumber()))
     .pipe(concat('style.less'))
     .pipe(less())
     .pipe(minify())
