6440b5dd4b2f9a231d63c7a5f893080941848788
timakin <timaki.st@gmail.com>
2015-06-21 17:49:58 +0900

    remove server.js task

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp       = require('gulp');
 var concat     = require('gulp-concat');
 var less       = require('gulp-less');
 var minify     = require('gulp-minify-css');
+var nodemon    = require('gulp-nodemon');
 var plumber    = require('gulp-plumber');
 var streamify  = require('gulp-streamify');
 var uglify     = require('gulp-uglify');
@@ -13,10 +14,6 @@ var path       = require('path');
 var source     = require('vinyl-source-stream');
 
 var config     = require('./config.json').build;
-
-var tasks = {
-  server: require('./tasks/server.js')
-};
 
 gulp.task('bundle:js', function() {
   browserify({
@@ -41,6 +38,29 @@ gulp.task('bundle:stylesheet', function() {
     .pipe(gulp.dest(config["DEST_DIR_PATH"]))
 });
 
-gulp.task('serve', tasks.server.serve);
-gulp.task('vmserve', tasks.server.vmserve);
+gulp.task('serve', function() {
+  nodemon({
+      script: './server/app.js',
+      ext: 'js html jsx',
+      ignore: ['bundle.js', 'style.css']
+  })
+  .on('change', ['js', 'less'])
+  .on('restart', function(){
+    console.log("restarted!");
+  });
+});
+
+gulp.task('vmserve', function() {
+  nodemon({
+      script: './server/app.js',
+      ext: 'js html jsx',
+      ignore: ['bundle.js', 'style.css'],
+      env: { 'NODE_ENV': 'vmdev' }
+  })
+  .on('change', ['js', 'less'])
+  .on('restart', function(){
+    console.log("restarted!");
+  });
+});
+
 gulp.task('default', ['bundle:stylesheet', 'bundle:js', 'serve']);
