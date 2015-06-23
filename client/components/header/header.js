80a5d45c4deb9e2977d1b2f48799e7ceae9435c0
Seiji Takahashi <seiji.takahashi@O-05467-MAC.local>
2015-06-23 09:33:59 +0900

    inline-css

--- client/components/header/header.js
+++ client/components/header/header.js
@@ -4,10 +4,15 @@ import React from 'react';
 import {Link, Route, RouteHandler} from 'react-router';
 import request from 'superagent';
 
+const styles = {
+  color: 'red',
+  backgroundColor: 'gray'
+}
+
 const Header = React.createClass({
   render(){
     return (
-      <div id="header">
+      <div id="header" style={styles}>
         <h1>header</h1>
         <Link to="/">Go Home</Link> | <Link to="/post">Go Post</Link>
       </div>
